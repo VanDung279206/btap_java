40c4651db9699ca4b869793c3562adb6c3269e62
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-07 14:07:01 +0700

    Bài 1: Cài đặt và vẽ lưu đồ thuật toán cho chương trình kiểm tra một số n có phải nguyên tố hay không?

---
+++ btap_java/src/ktra_ngto.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ktra_ngto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhập n: ");
+        int n = sc.nextInt();
+        int dem = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                dem++;
+            }
+        }
+        if (dem == 2) {
+            System.out.println("số " + n + " là số nguyên tố");
+        } else {
+            System.out.println("số " + n + " không phải là số nguyên tố");
+        }
+    }
+}
