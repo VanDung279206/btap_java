2e2569496e21f414afb8667ec7d532b6a1855755
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-11 00:06:32 +0700

    demo

--- btap_java/src/mang1chieu/insert.java
+++ btap_java/src/mang1chieu/insert.java
@@ -1,7 +1,20 @@
 package mang1chieu;
 
+import java.util.Random;
+
 public class insert {
     public static void main(String[] args) {
 
     }
+
+    //hàm tạo mảng
+    public static void mang(int[] arr){
+        Random rd = new Random();
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = rd.nextInt(101);
+        }
+    }
+
+    //hàm chèn
+
 }
