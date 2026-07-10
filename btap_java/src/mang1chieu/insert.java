aeb7d33df6d2871121c8b057c805f125806e80f6
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-11 00:20:57 +0700

    Cho một mảng đã được sắp xếp, viết chương trình bổ sung một giá trị mới vào mảng theo đúng thứ tự.

--- btap_java/src/mang1chieu/insert.java
+++ btap_java/src/mang1chieu/insert.java
@@ -1,20 +1,41 @@
 package mang1chieu;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class insert {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
+        System.out.print("mảng ban đầu: ");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+        System.out.println("\nnhập giá trị cần chèn: ");
+        int k = sc.nextInt();
 
-    }
+        //tìm vị trí cần chèn
+        int viTri = 0;
+        while (viTri < n && arr[viTri] < k) {
+            viTri++;
+        }
 
-    //hàm tạo mảng
-    public static void mang(int[] arr){
-        Random rd = new Random();
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = rd.nextInt(101);
+        //dịch phần tử sang phải
+        for (int i = n; i > viTri; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        //chèn giá trị
+        arr[viTri] = k;
+        n++;
+
+        System.out.print("mảng sau kh chèn: ");
+        for (int x : arr) {
+            System.out.print(x + " ");
         }
     }
-
-    //hàm chèn
-
 }
