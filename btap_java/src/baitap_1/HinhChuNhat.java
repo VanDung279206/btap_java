a0bfb07fc95052457d2853599bbef1d4d7cf0d66
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-27 21:00:31 +0700

    Xây dựng lớp: Bài toán hình chữ nhật

---
+++ btap_java/src/baitap_1/HinhChuNhat.java
@@ -0,0 +1,40 @@
+package baitap_1;
+
+import java.util.Scanner;
+
+public class HinhChuNhat {
+    static Scanner sc = new Scanner(System.in);
+    static double cd, cr;
+
+    public static void main(String[] args) {
+        double cd, cr;
+        nhap();
+        xuat();
+    }
+
+    // hàm nhập
+    public static void nhap() {
+        System.out.print("nhập chiều dài của HCN: ");
+        cd = sc.nextDouble();
+        System.out.print("nhập chiều rộng của HCN: ");
+        cr = sc.nextDouble();
+    }
+
+    //hàm tính chu vi
+    public static double tinhChuVi() {
+        return (cd + cr) * 2;
+    }
+
+    //hàm tính diện tích
+    public static double tinhDienTich() {
+        return cd * cr;
+    }
+
+    // hàm xuất
+    public static void xuat() {
+        System.out.println("chiều dài : " + cd +
+                "\nchiều rộng: " + cr +
+                "\nchu vi: " + tinhChuVi() +
+                "\ndiện tích: " + tinhDienTich());
+    }
+}
