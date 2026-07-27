f0e8293ad6a7a5681436cbcb3436dc420f866989
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-27 23:43:02 +0700

    nhân viên

---
+++ btap_java/src/baitap_5/TestMain.java
@@ -0,0 +1,11 @@
+package baitap_5;
+
+public class TestMain {
+    public static void main(String[] args) {
+        NhanVien nv = new NhanVien();
+
+        nv.nhap();
+
+        nv.xuat();
+    }
+}
