fe40c98b312f4750758d303c8a0eaa9a73f27aaa
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-28 07:36:30 +0700

    xây dựng lớp sản phẩm

---
+++ btap_java/src/y_1/SanPham.java
@@ -0,0 +1,37 @@
+package y_1;
+
+import java.util.Scanner;
+
+public class SanPham {
+
+    protected String maSanPham;
+    protected String tenSanPham;
+    protected double donGia;
+
+    public SanPham() {
+    }
+
+    public SanPham(String maSanPham, String tenSanPham, double donGia) {
+        this.maSanPham = maSanPham;
+        this.tenSanPham = tenSanPham;
+        this.donGia = donGia;
+    }
+
+    public void nhap() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhập mã sản phẩm: ");
+        maSanPham = sc.nextLine();
+
+        System.out.println("nhập tên sản phẩm: ");
+        tenSanPham = sc.nextLine();
+
+        System.out.println("nhập đơn giá: ");
+        donGia = Double.parseDouble(sc.nextLine());
+    }
+
+    public void xuat() {
+        System.out.println("mã sản phẩm: " + maSanPham);
+        System.out.println("tên sản phẩm: " + tenSanPham);
+        System.out.println("đơn giá: " + donGia);
+    }
+}
