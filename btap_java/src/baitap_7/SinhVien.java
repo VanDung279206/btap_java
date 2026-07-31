ffd62de2fe9e3fe2c3b0684a99b4467202ca6676
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-31 17:54:09 +0700

    hàm in tiêu đề và hàm in dữ liệu

--- btap_java/src/baitap_7/SinhVien.java
+++ btap_java/src/baitap_7/SinhVien.java
@@ -56,7 +56,7 @@ public class SinhVien {
         this.diemThucHanh = diemThucHanh;
     }
 
-    public double getDiemTB(){
+    public double getDiemTB() {
         return (diemLyThuyet + diemThucHanh) / 2;
     }
 
@@ -74,8 +74,19 @@ public class SinhVien {
         diemThucHanh = Double.parseDouble(sc.nextLine());
     }
 
+    //in tiêu đề
+    public static void inTieuDe() {
+        System.out.printf("%-20s %-20s %-25s %-25s %-25s %-20s\n",
+                "mã sinh viên", "họ tên", "điểm lý thuyết", "điểm thực hành", "điểm trung bình", "kết quả");
+    }
 
+    //in dữ liệu
+    public void inDuLieu(){
+        String ketQua = getDiemTB() >= 5 ? "Đậu" : "Trượt";
 
+        System.out.printf("%-20s %-20s %-25.2f %-25.2f %-25.2f %-20s\n",
+                maSinhVien, hoTen, diemLyThuyet, diemThucHanh, getDiemTB(), ketQua);
+    }
     @Override
     public String toString() {
         return super.toString();
