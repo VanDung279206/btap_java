26015eab9e5ab0797f860b4e366736d11b4fc7e6
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-31 20:14:20 +0700

    tạo lớp cha hình phẳng

---
+++ btap_java/src/baitap_8/HinhPhang.java
@@ -0,0 +1,8 @@
+package baitap_8;
+
+public abstract class HinhPhang {
+    public abstract void nhap();
+    public abstract double tinhChuVi();
+    public abstract double tinhDienTich();
+    public abstract void xuat();
+}
