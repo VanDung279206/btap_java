cab727fd520945266671ad96fa8be47c3968538e
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-31 20:22:01 +0700

    lớp con kế thừa lớp hình phẳng

---
+++ btap_java/src/baitap_8/HinhChuNhat.java
@@ -0,0 +1,39 @@
+package baitap_8;
+
+import java.util.Scanner;
+
+public class HinhChuNhat extends HinhPhang {
+    //thuộc tính
+    private double dai;
+    private double rong;
+
+    @Override
+    public void nhap() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("nhập chiều dài: ");
+        dai = sc.nextDouble();
+
+        System.out.print("nhập chiều rộng: ");
+        rong = sc.nextDouble();
+    }
+
+    @Override
+    public double tinhChuVi() {
+        return (dai + rong) / 2;
+    }
+
+    @Override
+    public double tinhDienTich() {
+        return (dai * rong);
+    }
+
+    @Override
+    public void xuat() {
+        System.out.println("======== HÌNH CHỮ NHẬT =======");
+        System.out.println("chiều dài: " + dai);
+        System.out.println("chiều rộng: " + rong);
+        System.out.println("chu vi: " + tinhChuVi());
+        System.out.println("diện tích: " + tinhDienTich());
+    }
+}
