7c77af989dd345bf76a5443a4c1b7f51057933ec
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-31 17:59:41 +0700

    nhập thông tin sinh viên 1 bằng hàm tao đủ tham số

--- btap_java/src/baitap_7/TestMain.java
+++ btap_java/src/baitap_7/TestMain.java
@@ -1,9 +1,28 @@
 package baitap_7;
 
+import java.util.Scanner;
+
 public class TestMain {
     public static void main(String[] args) {
-    SinhVien sv = new SinhVien();
-    sv.nhap();
+        Scanner sc = new Scanner(System.in);
+
+        //sinh viên 1 nhập bằng hàm tạo đủ tham số
+
+        System.out.println("nhập thông tin sinh viên 1");
+        System.out.print("mã sinh viên: ");
+        String ma = sc.nextLine();
+
+        System.out.print("họ tên sinh viên: ");
+        String ten = sc.nextLine();
+
+        System.out.print("điểm lý thuyết: ");
+        double dlt = Double.parseDouble(sc.nextLine());
+
+        System.out.print("điểm thực hành: ");
+        double dth = Double.parseDouble(sc.nextLine());
+
+        SinhVien sv1 = new SinhVien(ma, ten, dlt, dth);
+
 
     }
 }
