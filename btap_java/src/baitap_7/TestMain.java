0d8afe05a52738a349f32173d34718601031aa60
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-31 18:06:19 +0700

    tạo đối tượng sinh viên 3 rồi set + tạo list SinhVien

--- btap_java/src/baitap_7/TestMain.java
+++ btap_java/src/baitap_7/TestMain.java
@@ -25,5 +25,24 @@ public class TestMain {
 
         //sinh viên 2 giả trị cố định
         SinhVien sv2 = new SinhVien("2024607310", "Nguyễn Văn Dũng", 10, 01);
+
+        //sinh viên 3 tạo mặc định rồi set
+        SinhVien sv3 = new SinhVien();
+
+        System.out.println("nhập thông tin sinh viên 3");
+
+        System.out.print("mã sinh viên: ");
+        sv3.setMaSinhVien(sc.nextLine());
+
+        System.out.print("họ tên: ");
+        sv3.setHoTen(sc.nextLine());
+
+        System.out.print("điểm lý thuyết: ");
+        sv3.setDiemLyThuyet(Double.parseDouble(sc.nextLine()));
+
+        System.out.print("điểm thực hành: ");
+        sv3.setDiemThucHanh(Double.parseDouble(sc.nextLine()));
+
+        SinhVien[] ds = {sv1, sv2, sv3};
     }
 }
