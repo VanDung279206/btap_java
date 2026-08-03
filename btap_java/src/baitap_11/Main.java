98d2624a0e5d4259d6b3c38c525ff898d10d6812
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-03 10:11:13 +0700

    quan hệ kết tập

---
+++ btap_java/src/baitap_11/Main.java
@@ -0,0 +1,13 @@
+package baitap_11;
+
+public class Main {
+    public static void main(String[] args) {
+        HoaDon hd = new HoaDon();
+        hd.nhap();
+        System.out.println("========= HOA DON ===========");
+        hd.xuat();
+        hd.xapSep();
+        System.out.println("========= HOA DON SAU KHI XAP SEP ============");
+        hd.xuat();
+    }
+}
