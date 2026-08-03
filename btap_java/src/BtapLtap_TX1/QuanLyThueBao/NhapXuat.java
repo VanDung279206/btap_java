1c492ba8b744f11dff4c1a3b171512db4a53d4fd
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-03 23:33:16 +0700

    ke thua va giao dien

---
+++ btap_java/src/BtapLtap_TX1/QuanLyThueBao/NhapXuat.java
@@ -0,0 +1,11 @@
+package BtapLtap_TX1.QuanLyThueBao;
+
+import java.util.Scanner;
+
+public interface NhapXuat {
+    double TIEN_THUE_BAO = 3000;
+    double MUC_TRAN = 400000;
+
+    void nhap(Scanner sc);
+    void xuat();
+}
