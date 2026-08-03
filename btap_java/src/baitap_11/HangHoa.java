d8c14b343e9bbe364da4f1d8e69b64b79629821c
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-03 10:11:00 +0700

    quan hệ kết tập

--- btap_java/src/baitap_11/HangHoa.java
+++ btap_java/src/baitap_11/HangHoa.java
@@ -1,4 +1,53 @@
 package baitap_11;
 
+import java.util.Scanner;
+
 public class HangHoa {
+    // thuoc tinh
+    private String tenHang;
+    private double donGia;
+    private int soLuong;
+
+    //constructor
+
+    public HangHoa() {
+    }
+
+    public HangHoa(String tenHang, double donGia, int soLuong) {
+        this.tenHang = tenHang;
+        this.donGia = donGia;
+        this.soLuong = soLuong;
+    }
+
+    public String getTenHang() {
+        return tenHang;
+    }
+
+    public double getThanhTien() {
+        return donGia + soLuong;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    // ham nhap
+    public void nhap() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ten hang: ");
+        tenHang = sc.nextLine();
+
+        System.out.println("don gia: ");
+        donGia = Double.parseDouble(sc.nextLine());
+
+        System.out.println("so luong: ");
+        soLuong = Integer.parseInt(sc.nextLine());
+    }
+
+    //ham xuat
+    public void xuat() {
+        System.out.printf("%-15s %-10.0f %-10d %-10.0f\n",
+                tenHang, donGia, soLuong, getThanhTien());
+    }
 }
