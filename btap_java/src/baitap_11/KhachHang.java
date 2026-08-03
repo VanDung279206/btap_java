343345df5f47cc058252e19b45dc72bc96b2cde1
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-03 10:11:10 +0700

    quan hệ kết tập

--- btap_java/src/baitap_11/KhachHang.java
+++ btap_java/src/baitap_11/KhachHang.java
@@ -1,4 +1,35 @@
 package baitap_11;
 
+import java.util.Scanner;
+
 public class KhachHang {
+    //thuộc tính
+    private String tenKH;
+    private String diaChi;
+
+    //constructor
+
+    public KhachHang() {
+    }
+
+    public KhachHang(String tenKH, String diaChi) {
+        this.tenKH = tenKH;
+        this.diaChi = diaChi;
+    }
+
+    //hàm nhập
+    public void nhap() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("tên khách hàng: ");
+        tenKH = sc.nextLine();
+
+        System.out.println("dia chi: ");
+        diaChi = sc.nextLine();
+    }
+
+    //ham xuat
+    public void xuat() {
+        System.out.println("khach hang: " + tenKH + "\t\tDia chi: " + diaChi);
+    }
 }
