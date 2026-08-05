9b8ac46568a27d7e6bc80e40454664f29fe961f8
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-05 20:41:16 +0700

    ke thua va giao dien

---
+++ btap_java/src/BtapLtap_TX1/QuanLySanPham/TinhTien.java
@@ -0,0 +1,9 @@
+package BtapLtap_TX1.QuanLySanPham;
+
+public interface TinhTien {
+    double KHUYEN_MAI_TOI_DA = 0.2;
+    int NGUONG_SO_LUONG = 10;
+
+    void tinhTongTien();
+    void xuatHoaDon();
+}
