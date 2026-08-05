8fe34815b086c01c9bc1bd2ec9bf06557ef6beca
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-05 23:22:10 +0700

    btap luyen tap

---
+++ btap_java/src/BtapLtap_TX1/QuanLyHoaDonDien/KhachHangDien.java
@@ -0,0 +1,25 @@
+package BtapLtap_TX1.QuanLyHoaDonDien;
+
+import java.util.Scanner;
+
+public abstract class KhachHangDien {
+    protected String maKH;
+    protected String tenKH;
+
+    public KhachHangDien() {
+    }
+
+    public void nhap(Scanner sc) {
+        System.out.print("nhap ma khach hang: ");
+        maKH = sc.nextLine();
+
+        System.out.print("nhap ten khach hang: ");
+        tenKH = sc.nextLine();
+
+    }
+
+    public void xuat() {
+        System.out.printf("%-10s %-20s",
+                maKH, tenKH);
+    }
+}
