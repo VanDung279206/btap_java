015109d4ce32ca9c6297ade50d0b6768f5123631
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-06 23:02:30 +0700

    btap luyen tap

--- btap_java/src/BtapLtap_TX1/QuanLyHoaDonDien/HoaDonDien.java
+++ btap_java/src/BtapLtap_TX1/QuanLyHoaDonDien/HoaDonDien.java
@@ -2,7 +2,7 @@ package BtapLtap_TX1.QuanLyHoaDonDien;
 
 import java.util.Scanner;
 
-public class HoaDonDien extends KhachHangDien implements TieuThu{
+public class HoaDonDien extends KhachHangDien implements TieuThu {
     private double chiSoCu;
     private double chiSoMoi;
 
@@ -16,12 +16,37 @@ public class HoaDonDien extends KhachHangDien implements TieuThu{
         System.out.print("nhap chi so cu: ");
         chiSoCu = sc.nextDouble();
 
-        System.out.print("nhap chi so moi: ");
-        chiSoMoi = sc.nextDouble();
+        do {
+            System.out.print("nhap vao chi so moi: ");
+            chiSoMoi = sc.nextDouble();
+
+            if (chiSoMoi < chiSoCu) {
+                System.out.println("chi so moi khong duoc nho hon chi so cu");
+            }
+        } while (chiSoMoi < chiSoCu);
     }
 
     @Override
     public double tinhTienDien() {
-        return 0;
+        double soKW = chiSoMoi - chiSoCu;
+
+        if (soKW <= 50) {
+            return soKW * DON_GIA_BAC_1;
+        } else {
+            return (50 * DON_GIA_BAC_1) + ((soKW - 50) * DON_GIA_BAC_2);
+        }
+    }
+
+    @Override
+    public void xuat() {
+        super.xuat();
+
+        System.out.printf("%-10.0f %-10.0f %-15.0f\n",
+                chiSoCu, chiSoMoi, tinhTienDien());
+    }
+
+    public static void xuatTieuDe() {
+        System.out.printf("%-10s %-20s %-10s %-10s %-15s\n",
+                "Ma KH", "Ten KH", "CS Cu", "CS Moi", "Tong Tien");
     }
 }
