548113f4b9b8e2fc5f780706655e2dc2395430d5
VanDung279206 <nguyenvandung279206@gmail.com>
2026-08-13 20:08:03 +0700

    test

--- SwingApp/src/QuanLySinhVien.java
+++ SwingApp/src/QuanLySinhVien.java
@@ -197,7 +197,6 @@ public class QuanLySinhVien extends Frame implements ActionListener {
     /**
      * Xu ly khi bam nut "Dong":
      * Dong cua so va ket thuc chuong trinh.
-     *
      */
     private void xuLyDong() {
         setVisible(false);
